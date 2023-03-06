0340415823b2417ce060d27bf866faeee194bcfc
Tamas Pinter <p.tamas@ding.hu>
2023-03-06 14:56:38 +0100

    docs(hello): Add comment to hello

--- Az400Console/Program.cs
+++ Az400Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// This is a comment.
 Console.WriteLine("Hello, AZ-400!");
 
 Console.ReadKey();
